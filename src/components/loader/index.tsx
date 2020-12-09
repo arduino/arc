dedc09cc17e61a9740694622d6753092be656758
Francesco Stasi <f.stasi@me.com>
2020-12-09 11:57:06 +0100

    inherit color and loader documentation

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -4,9 +4,21 @@ import classnames from 'classnames';
 import style from './loader.module.scss';
 
 export type LoaderProps = {
+  /**
+   * When tiny, the loader is not absolute, but a standard div. This is useful to use it inside other elements.
+   * If not tiny, the loader is meant to be used as a "global" laoder for the entire page
+   */
   tiny?: boolean;
 };
 
+/**
+ * A generic loader with a rotational animation.
+ *
+ * If used with the tiny flag is meant to be included in other components (such as Buttons).
+ *
+ * The background is inherited from the containing div
+ *
+ */
 export function Loader({ tiny = false }: LoaderProps): React.ReactElement {
   return (
     <div className={classnames({ [`${style['tiny-loader']}`]: tiny, [`${style.loader}`]: !tiny })}>
